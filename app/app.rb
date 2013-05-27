22d5b6732264b56e4e7ca0f5932c7ce4313d9b47
Sumeet Singh <ortuna@gmail.com>
2013-05-26 23:37:17 -0400

    about page and highlight.js integration

--- app/app.rb
+++ app/app.rb
@@ -9,7 +9,16 @@ module Ortuna
 
     private
     def markdown_to_html(markdown, options = {})
-      options.reverse_merge!(:autolink => true, :space_after_headers => true)
+      options.reverse_merge!(
+        :autolink => true,
+        :gh_blockcode => true,
+        :fenced_code_blocks => true,
+        :no_intra_emphasis => true,
+        :autolink => true,
+        :strikethrough => true,
+        :lax_html_blocks => true,
+        :superscript => true
+      )
       parser = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
       parser.render(markdown)
     end
--- app/controllers/site.rb
+++ app/controllers/site.rb
@@ -4,6 +4,11 @@ Ortuna::App.controllers do
     render :index
   end
 
+  get :about do
+    @title = 'Sumeet Singh'
+    render :about
+  end
+
   get :article, map: '/:path' do
     @article = Article.first(published: true, base_path: "#{params[:path]}.md")
     @title   = @article[:title]
