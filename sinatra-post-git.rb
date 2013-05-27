73c3f7b0d2a1f7b0d5e5355a6cc8236bc92e35bf
Sumeet Singh <ortuna@gmail.com>
2013-05-26 22:34:34 -0400

    Right paths updated for post hooks

--- sinatra-post-git.rb
+++ sinatra-post-git.rb
@@ -5,8 +5,19 @@ set :bind, '0.0.0.0'
 set :port, 9000
 
 post '/' do
+
+end
+
+def update_origin
   Dir.chdir '../content'
   g = Grit::Repo.new('../content')
+  g.git.reset({:hard => true}, 'HEAD')
+  g.git.pull({}, "origin", "master")
+end
+
+def update_tmp
+  Dir.chdir '/tmp/ortuna-content'
+  g = Grit::Repo.new('/tmp/ortuna-content')
   g.git.reset({:hard => true}, 'HEAD')
   g.git.pull({}, "origin", "master")
 end
